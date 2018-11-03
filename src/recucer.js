247dbfa5930774771099262f9bc330b38764d6e3
stavalfi <stavalfi@gmail.com>
2018-11-03 23:58:23 +0200

    reducer.js - in every place I search for the first element in array by predicate, I use findIndex instead of filter. #10

--- src/recucer.js
+++ src/recucer.js
@@ -79,34 +79,36 @@ export default (actions, flowsFunctions, workflowsDetails) => (state, action) =>
     if (!isActionValid(actions, workflowsDetails, state.nonActiveWorkflowsDetails, action))
         return state;
 
-    const workflowDetails = workflowsDetails.filter(workflow => workflow.workflowName === action.workflowName);
+    const workflowDetailsIndex = workflowsDetails.findIndex(workflow => workflow.workflowName === action.workflowName);
 
-    if (workflowDetails.length === 0)
+    if (workflowDetailsIndex === -1)
     // the workflow does not exist by the given name in the action.
         return state;
 
-    const activeWorkflowDetails = state.activeWorkflowsDetails.filter(activeWorkflow => activeWorkflow.workflowId === action.workflowId);
+    const workflowDetails = workflowsDetails[workflowDetailsIndex];
 
-    if (activeWorkflowDetails.length === 0) {
+    const activeWorkflowDetailsIndex = state.activeWorkflowsDetails.findIndex(activeWorkflow => activeWorkflow.workflowId === action.workflowId);
+
+    if (activeWorkflowDetailsIndex === -1) {
         if (action.flowStatus !== flowStatuses.started)
         // the workflow has not stated yet and the user notify about finished flowStatus that is not the first flowStatus.
             return state;
-        if (workflowDetails[0].head.isJust() && action.flowName !== workflowDetails[0].head.value().flowDetails.flowName)
+        if (workflowDetails.head.isJust() && action.flowName !== workflowDetails.head.value().flowDetails.flowName)
         // the user didn't dispatch the first flow in this workflow.
             return state;
 
         // we need to create this workflow
-        const updatedHeadNode = workflowDetails[0].head.isNothing() ?
+        const updatedHeadNode = workflowDetails.head.isNothing() ?
             Maybe.Nothing :
             {
-                ...workflowDetails[0].head.value(),
+                ...workflowDetails.head.value(),
                 isCompleted: true,
                 completeTime: action.flowStatusCompleteTime
             };
         const newActiveWorkflowDetails = {
             workflowId: action.workflowId,
-            workflowName: workflowDetails[0].workflowName,
-            head: duplicateWorkflowGraph(workflowDetails[0].head, updatedHeadNode),
+            workflowName: workflowDetails.workflowName,
+            head: duplicateWorkflowGraph(workflowDetails.head, updatedHeadNode),
             workflowStatus: workflowStatuses.started
         };
         return loop(
@@ -114,15 +116,17 @@ export default (actions, flowsFunctions, workflowsDetails) => (state, action) =>
                 ...state,
                 activeWorkflowsDetails: [...state.activeWorkflowsDetails, newActiveWorkflowDetails],
             },
-            Cmd.list(getActionsToTrigger(actions, flowsFunctions, workflowDetails[0].head.value().childs, action))
+            Cmd.list(getActionsToTrigger(actions, flowsFunctions, workflowDetails.head.value().childs, action))
         );
     }
+
+    const activeWorkflowDetails = state.activeWorkflowsDetails[activeWorkflowDetailsIndex];
 
     // find the node of the flow that is completed.
 
     // Note: execution of the same flow in parallel is not supported so there
     //       is no way that the same flow will be more then one in the following array.
-    const uncompletedNodes = getCurrentLeafsOfWorkflowGraph(activeWorkflowDetails[0].head);
+    const uncompletedNodes = getCurrentLeafsOfWorkflowGraph(activeWorkflowDetails.head);
 
     const completedNodeIndex = uncompletedNodes.findIndex(node => node.flowDetails.flowName === action.flowName);
 
@@ -137,7 +141,7 @@ export default (actions, flowsFunctions, workflowsDetails) => (state, action) =>
     };
     const updatedActiveWorkflowDetails = {
         workflowId: action.workflowId,
-        head: duplicateWorkflowGraph(activeWorkflowDetails[0].head, updatedNode),
+        head: duplicateWorkflowGraph(activeWorkflowDetails.head, updatedNode),
         workflowStatus: workflowStatuses.started
     };
 
