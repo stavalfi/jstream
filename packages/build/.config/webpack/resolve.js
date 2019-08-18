97c479d5a8d62b83240f08230bf4240075b61944
stavalfi <stav.alfi@stratoscale.com>
2019-08-18 12:23:46 +0300

    wip

--- packages/build/.config/webpack/resolve.js
+++ packages/build/.config/webpack/resolve.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const { webpackDevelopmentAlias, webpackOtherAlias, webpackProdAlias } = require('../utils/paths-resolving-strategies')
 
 module.exports = ({ isDevelopmentMode, paths: { resolveModulesPathsArray }, constants: {} }) => {
@@ -10,5 +11,25 @@ module.exports = ({ isDevelopmentMode, paths: { resolveModulesPathsArray }, cons
       ...webpackOtherAlias,
       ...(isDevelopmentMode && { 'react-dom': '@hot-loader/react-dom' }),
     },
+    plugins: [new MyConventionResolver()],
   }
+}
+
+function MyConventionResolver(source, target) {
+  this.source = source || 'resolve'
+  this.target = target || 'resolve'
+}
+
+MyConventionResolver.prototype.apply = function(resolver) {
+  const target = resolver.ensureHook(this.target)
+  resolver.getHook(this.source).tapAsync('MyConventionResolver', function(request, resolveContext, callback) {
+    if (request.request[0] === '#') {
+      const req = request.request.substr(1)
+      const obj = Object.assign({}, request, {
+        request: req + '/' + path.basename(req) + '.js',
+      })
+      return resolver.doResolve(target, obj, null, resolveContext, callback)
+    }
+    callback()
+  })
 }
